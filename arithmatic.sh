890c8dadd0ff5e572474927fb456fb0f249cfc07
supriyaphalle <supriyaphalle@gmail.com>
2020-01-30 14:56:16 +0530

    ADDED: first computation a+b*c

--- arithmatic.sh
+++ arithmatic.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -x
+
 echo " Welcome to the Sorting and arithmatic computations"
 
 read -p 'Enter first number: ' a;
 read -p 'enter 2nd number: ' b;
 read -p 'enter 3rd number: ' c;
 
+result1=$(( $a + $b * $c))
+echo $result1;
