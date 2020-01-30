64af7b9ebfd2da20b4ec2acab94b0c9c98948d55
supriyaphalle <supriyaphalle@gmail.com>
2020-01-30 15:20:24 +0530

    ADDED: Third computation c+a/b

--- arithmatic.sh
+++ arithmatic.sh
@@ -6,9 +6,12 @@ read -p 'Enter first number: ' a;
 read -p 'enter 2nd number: ' b;
 read -p 'enter 3rd number: ' c;
 
-result1=$(( $a + $b * $c))
+result1=`echo "$a + $b * $c" | bc`
 echo $result1;
 
-result2=$(( $a * $b + $c))
+result2=`echo "$a * $b + $c" | bc`
 echo $result2;
+
+result3=`echo "scale=2; $c + $a / $b" | bc`
+echo $result3;
 
