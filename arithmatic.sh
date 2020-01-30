2ebfd99d6c4343d667d2c7d89562c7531203cb74
supriyaphalle <supriyaphalle@gmail.com>
2020-01-30 16:33:18 +0530

    ADDED: Added array to store data from dictionary

--- arithmatic.sh
+++ arithmatic.sh
@@ -4,16 +4,31 @@ echo " Welcome to the Sorting and arithmatic computations"
 
 declare -A Results
 
+
+FirstOperation=1;
+SecondOperation=2;
+ThirdOperation=3;
+ForthOperation=4;
+
 read -p 'Enter first number: ' a;
 read -p 'enter 2nd number: ' b;
 read -p 'enter 3rd number: ' c;
 
-Results[result1]=`echo "$a + $b * $c" | bc`
+Results[$FirstOperation]=`echo "$a + $b * $c" | bc`
 
-Results[result2]=`echo "$a * $b + $c" | bc`
+Results[$SecondOperation]=`echo "$a * $b + $c" | bc`
 
-Results[result3]=`echo "scale=2; $c + $a / $b" | bc`
+Results[$ThirdOperation]=`echo "scale=2; $c + $a / $b" | bc`
 
-Results[result4]=`echo " $a % $b +$c" | bc`
+Results[$ForthOperation]=`echo " $a % $b +$c" | bc`
 
-echo ${Results[@]}
+echo "Direct Result through Dictionary ${Results[@]}"
+
+
+for ((i=1;i<5;i++))
+do
+		a[i]=${Results[$i]}
+done
+
+
+echo "Result through Array:${a[@]} "
